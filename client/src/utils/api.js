c73dc60ba270eedbe5035286627396abde12921d
Evan-Codes88 <Evan2401tm@gmail.com>
2025-04-25 18:06:22 +1000

    Feat: Fixing things for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://portfolio-backend-nhmf.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://portfolio-backend-nhmf.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
