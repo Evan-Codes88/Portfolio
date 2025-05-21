70728709c67191e345a70af8c17ce722d3525246
Evan-Codes88 <Evan2401tm@gmail.com>
2025-05-21 22:37:13 +1000

    Feat: Updating loader to use absolute positioning

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,13 +3,16 @@ import { motion } from "framer-motion";
 
 const Loader = () => (
   <motion.div
-    className="flex justify-center items-center h-screen"
+    className="relative h-screen w-screen"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
   >
-    <div className="w-16 h-16 border-4 border-pink-400 border-t-transparent rounded-full animate-spin"></div>
+    <div className="absolute top-1/4 left-1/2 transform -translate-x-1/4">
+      <div className="w-16 h-16 border-4 border-pink-400 border-t-transparent rounded-full animate-spin"></div>
+    </div>
   </motion.div>
 );
 
 export default Loader;
+
 
